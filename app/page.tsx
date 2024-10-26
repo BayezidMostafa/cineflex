042047403f8181db3c3df0654c2fa884b8bd82fd
Bayezid <hiperx1@gmail.com>
2024-10-26 23:03:47 +0600

    search route issue

--- app/page.tsx
+++ app/page.tsx
@@ -151,7 +151,7 @@ const Home = () => {
         </div>
       </div>
       {error && <h2>Failed to load data</h2>}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 md:gap-6 lg:gap-8 mt-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 md:gap-6 lg:gap-8 mt-5">
         {movies.map((m, i) => (
           <Card key={i} data={m} />
         ))}
